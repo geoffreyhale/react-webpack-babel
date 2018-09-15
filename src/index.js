35badde3a845b37d545207c24638af51eddf70e8
geoffreyhale <geoffreyhale@gmail.com>
2018-09-15 16:52:12 -0700

    add first component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HelloWorld from "./components/HelloWorld";
 
 const Index = () => {
-  return <div>Hello React!</div>;
+  return <div><HelloWorld /></div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
