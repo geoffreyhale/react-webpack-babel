ef5eab29105468d3ef254eac9c36092425056a40
geoffreyhale <geoffreyhale@gmail.com>
2018-09-22 16:53:35 -0700

    use AppStore to list store keys on Home page

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { inject, observer } from 'mobx-react';
 import { PageHeader } from 'react-bootstrap';
 
 import HelloWorld from '../../shared/HelloWorld';
 import MyButton from '../../shared/MyButton';
 
-const Home = () => {
-    return (
-        <div>
-            <PageHeader>Home</PageHeader>
-            <HelloWorld />
-            <MyButton />
-        </div>
-    );
-};
+@inject('appStore')
+@observer
+class Home extends React.Component {
+    static propTypes = {
+        appStore: PropTypes.object
+    };
+
+    render() {
+        const { appStore } = this.props;
+
+        return (
+            <div>
+                <PageHeader>Home</PageHeader>
+                <HelloWorld />
+                <MyButton />
+                <div>
+                    <h3>appStore keys</h3>
+                    <ul>
+                        {Object.keys(appStore).map(k => (
+                            <li>{k}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Home;
