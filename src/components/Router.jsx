c0e0e4ce6e8c2f8dc695a6b3424b60ba8180328c
geoffreyhale <geoffreyhale@gmail.com>
2018-09-22 15:11:46 -0700

    use Grid as page container

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { Grid } from 'react-bootstrap';
 import Menu from './Menu';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,10 +10,11 @@ const Router = () => (
     <BrowserRouter>
         <div>
             <Menu />
-
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/blog" component={Blog} />
+            <Grid>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/blog" component={Blog} />
+            </Grid>
         </div>
     </BrowserRouter>
 );
